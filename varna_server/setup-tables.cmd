636e3b15851442d636455c0acd7fcb646be7b40c
Yurii <udinodessa@gmail.com>
2023-09-26 11:08:07 +0300

    first

---
+++ varna_server/setup-tables.cmd
@@ -0,0 +1,22 @@
+@echo off
+
+@REM Start docker
+echo Starting docker
+docker compose up --build --detach
+
+:LOOP
+netstat -o -n -a | >nul findstr "8090" && (
+    echo Waiting for Postgres...
+    timeout /t 2 /nobreak > NUL
+    goto :PORT_FOUND
+)
+echo Waiting for Postgres...
+goto :LOOP
+
+:PORT_FOUND
+echo Postgres is ready
+type .\generated\tables-serverpod.pgsql | docker compose run -T postgres env PGPASSWORD="Stik05MQbHIhgrVVeK2JRc1g9dy3JZHe" psql -h postgres -U postgres -d varna
+echo Stopping docker
+docker compose stop
+
+exit /b
